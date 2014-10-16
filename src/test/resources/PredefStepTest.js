65f56c45c4c6bf704dac59886419a01ba18019e6
Andrew Branson <andrew.branson@cern.ch>
2014-10-16 17:35:31 +0200

    Improved predefined step test script

--- src/test/resources/PredefStepTest.js
+++ src/test/resources/PredefStepTest.js
@@ -18,6 +18,7 @@
  *
  * http://www.fsf.org/licensing/licenses/lgpl.html
  */
+ 
 var agent = org.cristalise.gui.MainFrame.userAgent;
 var serverPath=new org.cristalise.kernel.lookup.DomainPath("/servers/pcuwe04.cern.ch");
 var serverItem=proxy.getProxy(serverPath);
@@ -26,11 +27,12 @@ var predef = "AddDomainContext"; var params = new Array(1); params[0] = "/test/c
 var predef = "RemoveDomainContext"; agent.execute(serverItem, predef, params);
 params[0] = "/test"; agent.execute(serverItem, predef, params);
 var predef = "SetAgentPassword"; params = Array(1); params[0] = "hunter2"; agent.execute(agent, predef, params); 
-org.cristalise.kernel.process.Gateway.login("dev", "hunter2");
-var predef = "SetAgentRoles"; agent.execute(serverItem, predef, params); //Role shouldn't exist
+org.cristalise.kernel.process.Gateway.getAuthenticator().authenticate("dev", "hunter2", "");
+params[0] = "test"; agent.execute(agent, predef, params);
+try { var predef = "SetAgentRoles"; agent.execute(serverItem, predef, params); throw "Test Role shouldn't exist"; } catch (e) { }
+//Role shouldn't exist
 params = Array(2); params[0] = "Admin"; params[1] = "User"; agent.execute(agent, predef, params);
 params = Array(1); params[0] = "Admin"; agent.execute(agent, predef, params);
-var predef = "RemoveAgent"; var params = new Array(0); agent.execute(agent, predef, params);
 
 var predef = "AddNewCollectionDescription"; var params = new Array(2);
 params[0] = "TestAgg"; params[1] = "Aggregation"; agent.execute(serverItem, predef, params);
@@ -45,6 +47,12 @@ params[0] = "TestAgg"; params[1] = "/desc/dev/ScriptFactory"; agent.execute(serv
 var predef = "AddMemberToCollection"; var params = new Array(2);
 params[0] = "TestDep"; params[1] = "/desc/dev/ScriptFactory"; agent.execute(serverItem, predef, params);
 
+var predef = "AddMemberToCollection"; var params = new Array(3);
+params[0] = "TestDep"; params[1] = "/desc/dev/SchemaFactory";
+params[2] = new org.cristalise.kernel.utils.CastorHashMap();
+params[2].put("TestProperty", true);
+agent.execute(serverItem, predef, params);
+
 var predef = "AssignItemToSlot"; var params = new Array(3);
 params[0] = "TestAgg"; params[1] = "0"; params[2] = "/desc/Script/system/dev/CreateNewNumberedVersionFromLast"; agent.execute(serverItem, predef, params);
 
@@ -56,3 +64,5 @@ params[0] = "TestAgg"; params[1] = "0"; agent.execute(serverItem, predef, params
 
 var params = new Array(2); params[0] = "TestDep"; params[1] = "-1"; params[2] = "/desc/dev/ScriptFactory";
 agent.execute(serverItem, predef, params);
+
+var predef = "RemoveAgent"; var params = new Array(0); agent.execute(agent, predef, params);
