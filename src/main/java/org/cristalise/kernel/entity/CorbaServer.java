a5147c236444bb12d829744c0ce589301e8db534
Andrew Branson <andrew.branson@cern.ch>
2015-09-29 16:23:58 +0200

    Revert "Removed WrongPolicy catch block to compile"
    This reverts commit b57e69e20d5d18d55b55307b401e8c74f6ac650e.

--- src/main/java/org/cristalise/kernel/entity/CorbaServer.java
+++ src/main/java/org/cristalise/kernel/entity/CorbaServer.java
@@ -184,7 +184,12 @@ public class CorbaServer {
     public TraceableEntity createItem(ItemPath itemPath) throws CannotManageException, ObjectAlreadyExistsException {
 
     	if (itemPath.exists()) throw new ObjectAlreadyExistsException();
-    	org.omg.CORBA.Object obj = mItemPOA.create_reference_with_id(itemPath.getOID(), ItemHelper.id());
+    	org.omg.CORBA.Object obj;
+    	try {
+    		obj = mItemPOA.create_reference_with_id(itemPath.getOID(), ItemHelper.id());
+		} catch (WrongPolicy e) {
+			throw new CannotManageException(e.getMessage());
+		}
         itemPath.setIOR(obj);
         TraceableEntity item = new TraceableEntity(itemPath, mItemPOA);
         synchronized (mItemCache) {
@@ -195,7 +200,12 @@ public class CorbaServer {
     
     public ActiveEntity createAgent(AgentPath agentPath) throws CannotManageException, ObjectAlreadyExistsException {
     	if (agentPath.exists()) throw new ObjectAlreadyExistsException();
-        org.omg.CORBA.Object obj = mAgentPOA.create_reference_with_id(agentPath.getOID(), AgentHelper.id());
+        org.omg.CORBA.Object obj;
+		try {
+			obj = mAgentPOA.create_reference_with_id(agentPath.getOID(), AgentHelper.id());
+		} catch (WrongPolicy e) {
+			throw new CannotManageException(e.getMessage());
+		}
         agentPath.setIOR(obj);
         ActiveEntity agent;
 		agent = new ActiveEntity(agentPath, mAgentPOA);
