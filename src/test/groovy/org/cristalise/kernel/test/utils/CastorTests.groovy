0ee44af30acff277042aa5796ffc7887313410d2
Zsolt Kovacs <zs.myth@gmail.com>
2015-10-29 12:20:44 +0100

    Licence header was incorrect

--- src/test/groovy/org/cristalise/kernel/test/utils/CastorTests.groovy
+++ src/test/groovy/org/cristalise/kernel/test/utils/CastorTests.groovy
@@ -1,6 +1,6 @@
 /**
  * This file is part of the CRISTAL-iSE kernel.
- * Copyright (c) 2001-2015 The CRISTAL Consortium. All rights reserved.
+ * Copyright (c) 2001-2014 The CRISTAL Consortium. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
