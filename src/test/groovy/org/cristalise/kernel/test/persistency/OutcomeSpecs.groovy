0acc39963f92e42fec9212bb32e2785d55fe7aa3
Zsolt Kovacs <zs.myth@gmail.com>
2015-10-29 12:14:48 +0100

    adding 2 groovy tests

---
+++ src/test/groovy/org/cristalise/kernel/test/persistency/OutcomeSpecs.groovy
@@ -0,0 +1,57 @@
+/**
+ * This file is part of the CRISTAL-iSE kernel.
+ * Copyright (c) 2001-2014 The CRISTAL Consortium. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * http://www.fsf.org/licensing/licenses/lgpl.html
+ */
+package org.cristalise.kernel.test.persistency
+
+import org.cristalise.kernel.persistency.outcome.Outcome
+import org.w3c.dom.Document
+
+import spock.lang.Specification
+
+
+/**
+ *
+ */
+class OutcomeSpecs extends Specification {
+
+    def 'Outcome C2KLocalObject can be constructed from path - /TestSchema/0/7'() {
+        when:
+        Outcome o = new Outcome("/TestSchema/0/7", (Document)null)
+
+        then:
+        o.getClusterType() == "Outcome"
+        o.getName() == "7";
+        o.getID() == 7;
+        o.getSchemaType() == "TestSchema";
+        o.getSchemaVersion() == 0;
+    }
+
+    def 'Outcome C2KLocalObject can be constructed from path - /Outcome/TestSchema/0/7'() {
+        when:
+        Outcome o = new Outcome("/Outcome/TestSchema/0/7", (Document)null)
+
+        then:
+        o.getClusterType() == "Outcome"
+        o.getName() == "7";
+        o.getID() == 7;
+        o.getSchemaType() == "TestSchema";
+        o.getSchemaVersion() == 0;
+    }
+}
---
+++ src/test/groovy/org/cristalise/kernel/test/utils/CastorTests.groovy
@@ -0,0 +1,52 @@
+/**
+ * This file is part of the CRISTAL-iSE kernel.
+ * Copyright (c) 2001-2015 The CRISTAL Consortium. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * http://www.fsf.org/licensing/licenses/lgpl.html
+ */
+package org.cristalise.kernel.test.utils
+
+import groovy.transform.CompileStatic
+
+import org.cristalise.kernel.common.InvalidDataException
+import org.cristalise.kernel.process.Gateway
+import org.cristalise.kernel.test.process.MainTest
+import org.cristalise.kernel.utils.CastorXMLUtility
+import org.cristalise.kernel.utils.FileStringUtility
+import org.cristalise.kernel.utils.Logger
+import org.junit.Before
+import org.junit.Test
+
+
+/**
+ *
+ */
+@CompileStatic
+class CastorTests {
+
+    @Before
+    public void setup() throws InvalidDataException {
+        Logger.addLogStream(System.out, 1);
+        Gateway.init( FileStringUtility.loadConfigFile(MainTest.class.getResource("/server.conf").getPath()) );
+    }
+
+    @Test
+    public void testMapFiles() throws Exception {
+        //TODO: this test needs to be rewritten
+        new CastorXMLUtility(Gateway.getResource(), Gateway.getProperties(), Gateway.getResource().getKernelResourceURL("mapFiles/"));
+    }
+}
