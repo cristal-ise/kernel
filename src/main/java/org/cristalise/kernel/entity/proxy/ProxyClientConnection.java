b3436abf2db6c8d9a3f2897a42dac2f7fca56a72
Zsolt Kovacs <zs.myth@gmail.com>
2016-07-26 15:01:38 +0200

    Code reformatted, imporved log messages and JavaDoc - done for #106

--- src/main/java/org/cristalise/kernel/entity/proxy/ProxyClientConnection.java
+++ src/main/java/org/cristalise/kernel/entity/proxy/ProxyClientConnection.java
@@ -36,58 +36,52 @@ import org.cristalise.kernel.process.Gateway;
 import org.cristalise.kernel.utils.Logger;
 import org.cristalise.kernel.utils.server.SocketHandler;
 
-
-/**************************************************************************
+/**
+ * 
  *
- * $Revision: 1.18 $
- * $Date: 2005/05/10 11:40:09 $
- *
- * Copyright (C) 2003 CERN - European Organization for Nuclear Research
- * All rights reserved.
- **************************************************************************/
-
+ */
 public class ProxyClientConnection implements SocketHandler {
-
-	Socket clientSocket = null;
-    static int clientId = -1;
-    int thisClientId;
+    static int          clientId = -1;
+    Socket              clientSocket = null;
+    int                 thisClientId;
     ArrayList<ItemPath> subscribedItems;
-    PrintWriter response;
-    BufferedReader request;
-    boolean closing = false;
+    PrintWriter         response;
+    BufferedReader      request;
+    boolean             closing = false;
 
     public ProxyClientConnection() {
         super();
         thisClientId = ++clientId;
         Gateway.getProxyServer().registerProxyClient(this);
-        Logger.msg(1, "Proxy Client Connection Handler "+thisClientId+" ready.");
+        Logger.msg(1, "ProxyClientConnection() - clientID:"+thisClientId+" READY.");
     }
 
-
     @Override
-	public String getName() {
+    public String getName() {
         return "Proxy Client Connection";
     }
 
     @Override
-	public boolean isBusy() {
+    public boolean isBusy() {
         return clientSocket != null;
     }
 
     @Override
-	public synchronized void setSocket(Socket newSocket) {
+    public synchronized void setSocket(Socket newSocket) {
         try {
-            Logger.msg(1, "Proxy Client Connection "+thisClientId+" connect from "+newSocket.getInetAddress()+":"+newSocket.getPort());
+            Logger.msg(1, "ProxyClientConnection.setSocket() - clientID "+thisClientId+" connect from "+newSocket.getInetAddress()+":"+newSocket.getPort());
             newSocket.setSoTimeout(500);
             clientSocket = newSocket;
             response = new PrintWriter(clientSocket.getOutputStream(), true);
             subscribedItems = new ArrayList<ItemPath>();
-        } catch (SocketException ex) {
-            Logger.msg("Could not set socket timeout:");
+        } 
+        catch (SocketException ex) {
+            Logger.error("ProxyClientConnection.setSocket() - Could not set socket timeout:");
             Logger.error(ex);
             closeSocket();
-        } catch (IOException ex) {
-            Logger.msg("Could not setup output stream:");
+        }
+        catch (IOException ex) {
+            Logger.error("ProxyClientConnection.setSocket() - Could not setup output stream:");
             Logger.error(ex);
             closeSocket();
         }
@@ -97,9 +91,11 @@ public class ProxyClientConnection implements SocketHandler {
      * Main loop. Reads proxy commands from the client and acts on them.
      */
     @Override
-	public void run() {
-        Thread.currentThread().setName("Proxy Client Connection: "+clientSocket.getInetAddress());
-        Logger.msg(7, "ProxyClientConnection "+thisClientId+" - Setting up proxy client connection with "+clientSocket.getInetAddress());
+    public void run() {
+        Thread.currentThread().setName(getName()+": "+clientSocket.getInetAddress());
+
+        Logger.msg(7, "ProxyClientConnection.run() - clientID:"+thisClientId+" - Setting up proxy client connection with "+clientSocket.getInetAddress());
+
         try {
             request = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String input = null;
@@ -107,102 +103,111 @@ public class ProxyClientConnection implements SocketHandler {
             while (clientSocket != null) {
                 try {
                     input = request.readLine();
-                    Logger.msg(9, "ProxyClientConnection "+thisClientId+" - received "+input);
+                    Logger.msg(9, "ProxyClientConnection.run() - "+thisClientId+" - received "+input);
                     thisMessage = new ProxyMessage(input);
                     processMessage(thisMessage);
-                } catch (InterruptedIOException ex) { //timeout
-                } catch (InvalidDataException ex) { // invalid proxy message
-                    Logger.error("ProxyClientConnection "+thisClientId+" - Invalid proxy message: "+input);
+                } 
+                catch (InterruptedIOException ex) { //timeout
                 }
-
+                catch (InvalidDataException ex) { // invalid proxy message
+                    Logger.error("ProxyClientConnection.run() - clientID:"+thisClientId+" - Invalid proxy message: "+input);
+                }
             }
-        } catch (IOException ex) {
+        }
+        catch (IOException ex) {
             if (!closing)
-                Logger.error("ProxyClientConnection "+thisClientId+" - Error reading from socket.");
+                Logger.error("ProxyClientConnection.run() - clientID:"+thisClientId+" - Error reading from socket.");
         }
         closeSocket();
-        Logger.msg(1, "ProxyClientConnection "+thisClientId+" closed.");
+        Logger.msg(1, "ProxyClientConnection.run() - clientID:"+thisClientId+" closed.");
     }
 
+    /**
+     * Processing a single message
+     * 
+     * @param message the message to be processed
+     * @throws InvalidDataException
+     */
     private void processMessage(ProxyMessage message) throws InvalidDataException {
-
         // proxy disconnection
         if (message.getPath().equals(ProxyMessage.BYEPATH)) {
-            Logger.msg(7, "ProxyClientConnection "+thisClientId+" disconnecting");
+            Logger.msg(7, "ProxyClientConnection.processMessage() - clientID:"+thisClientId+" disconnecting");
             closeSocket();
         }
-
-        // proxy checking connection
-        else if (message.getPath().equals(ProxyMessage.PINGPATH))
-        response.println(ProxyMessage.pingMessage);
-
-        // new subscription to entity changes
+        else if (message.getPath().equals(ProxyMessage.PINGPATH)) {
+            // proxy checking connection
+            response.println(ProxyMessage.pingMessage);
+        }
         else if (message.getPath().equals(ProxyMessage.ADDPATH)) {
-            Logger.msg(7, "ProxyClientConnection "+thisClientId+" subscribed to "+message.getItemPath());
+            // new subscription to entity changes
+            Logger.msg(7, "ProxyClientConnection.processMessage() - clientID:"+thisClientId+" subscribed to "+message.getItemPath());
+
             synchronized (subscribedItems) {
-            	subscribedItems.add(message.getItemPath());
+                subscribedItems.add(message.getItemPath());
             }
         }
-
-        // remove of subscription to entity changes
         else if (message.getPath().equals(ProxyMessage.DELPATH)) {
+            // remove of subscription to entity changes
             synchronized (subscribedItems) {
-            	subscribedItems.remove(message.getItemPath());
+                subscribedItems.remove(message.getItemPath());
             }
-            Logger.msg(7, "ProxyClientConnection "+thisClientId+" unsubscribed from "+message.getItemPath());
+            Logger.msg(7, "ProxyClientConnection.processMessage() - clientID:"+thisClientId+" unsubscribed from "+message.getItemPath());
         }
-
-        else // unknown message
-            Logger.error("ProxyClientConnection "+thisClientId+" - Unknown message type: "+message);
-
+        else {
+            // unknown message
+            Logger.error("ProxyClientConnection.processMessage() - clientID:"+thisClientId+" - Unknown message type: "+message);
+        }
     }
 
     public synchronized void sendMessage(ProxyMessage message) {
         if (clientSocket==null) return; // idle
         boolean relevant = message.getItemPath() == null;
+
         synchronized (subscribedItems) {
             for (Iterator<ItemPath> iter = subscribedItems.iterator(); iter.hasNext() && !relevant;) {
                 ItemPath thisKey = iter.next();
-                if (thisKey.equals(message.getItemPath()))
-                    relevant = true;
+
+                if (thisKey.equals(message.getItemPath())) relevant = true;
             }
         }
+
         if (!relevant) return; // not for our client
 
         response.println(message);
     }
 
     @Override
-	public void shutdown() {
+    public void shutdown() {
         if (isBusy()) {
             closing = true;
-            Logger.msg("ProxyClientConnection "+thisClientId+" closing.");
+            Logger.msg("ProxyClientConnection.shutdown() - clientID: "+thisClientId+" closing.");
             closeSocket();
         }
     }
 
     @Override
-	public String toString() {
+    public String toString() {
         if (clientSocket == null) return thisClientId+": idle";
         else return thisClientId+": "+clientSocket.getInetAddress();
     }
 
     private synchronized void closeSocket() {
         if (clientSocket==null) return;
+
         try {
             request.close();
             response.close();
             clientSocket.close();
-        } catch (IOException e) {
-            Logger.error("ProxyClientConnection "+thisClientId+" - Could not close socket.");
+        }
+        catch (IOException e) {
+            Logger.error("ProxyClientConnection.closeSocket() - clientID: "+thisClientId+" - Could not close socket.");
             Logger.error(e);
         }
+
         synchronized (subscribedItems) {
-        	subscribedItems = null;
+            subscribedItems = null;
         }
 
         clientSocket = null;
-
     }
-
 }
