969c7ecd9bf36253c90fd042ee19ccb958f870c5
Zsolt Kovacs <zs.myth@gmail.com>
2016-08-03 12:32:57 +0200

    - Uses lombok annotations: @Accessors(prefix = "m") @Getter @Setter - code reformatted - added method getTimeStampDate()

--- src/main/java/org/cristalise/kernel/events/Event.java
+++ src/main/java/org/cristalise/kernel/events/Event.java
@@ -21,6 +21,11 @@
 package org.cristalise.kernel.events;
 
 import java.util.Calendar;
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
 import org.cristalise.kernel.common.GTimeStamp;
 import org.cristalise.kernel.common.InvalidDataException;
@@ -35,285 +40,111 @@ import org.cristalise.kernel.persistency.outcome.Schema;
 import org.cristalise.kernel.utils.Logger;
 
 
-
 /**
  * The data structure of events, which are passed over the event service.
  *
- * Events are incrementaly numbered objects maintained by the History.
- *
- * @version $Revision: 1.13 $ $Date: 2004/11/22 09:12:28 $
- * @author  $Author: abranson $
+ * Events are incrementally numbered objects maintained by the History.
  */
-public class Event implements C2KLocalObject
-{
-	ItemPath mItemPath; AgentPath mAgentPath, mDelegatePath;
-	int mOriginState, mTransition, mTargetState;
-	Integer mID, mSchemaVersion, mStateMachineVersion;
+@Accessors(prefix = "m") @Getter @Setter
+public class Event implements C2KLocalObject {
+    ItemPath mItemPath;
+    AgentPath mAgentPath, mDelegatePath;
+    int mOriginState, mTransition, mTargetState;
+    Integer mID, mSchemaVersion, mStateMachineVersion;
     String mName, mStepName, mStepPath, mStepType, mSchemaName, mStateMachineName, mViewName, mAgentRole;
     GTimeStamp mTimeStamp;
-    
+
     public Event(ItemPath itemPath, 
-    		AgentPath agentPath, AgentPath delegatePath, String agentRole,
-    		String stepName, String stepPath, String stepType,
-            StateMachine stateMachine, int transitionId) {
-    	
-    	Transition transition = stateMachine.getTransition(transitionId);
-		Logger.msg(7, "History.addEvent() - creating new event for "+transition.getName()+" on "+stepName+" in "+mItemPath);
-		setItemPath(itemPath);
-		setAgentPath(agentPath);
-		setDelegatePath(delegatePath);
-		setAgentRole(agentRole);
-		setStepName(stepName);
-		setStepPath(stepPath);
-		setStepType(stepType);
-		setTransition(transitionId);
-		setOriginState(transition.getOriginStateId());
-		setTargetState(transition.getTargetStateId());
-		setStateMachineName(stateMachine.getItemID());
-		setStateMachineVersion(stateMachine.getVersion());
-		setTimeStamp(Event.getGMT());
+                 AgentPath agentPath, AgentPath delegatePath, String agentRole,
+                 String stepName, String stepPath, String stepType,
+                 StateMachine stateMachine, int transitionId)
+    {
+        Transition transition = stateMachine.getTransition(transitionId);
+        Logger.msg(7, "Event() - creating new event for "+transition.getName()+" on "+stepName+" in "+mItemPath);
+
+        setItemPath(itemPath);
+        setAgentPath(agentPath);
+        setDelegatePath(delegatePath);
+        setAgentRole(agentRole);
+        setStepName(stepName);
+        setStepPath(stepPath);
+        setStepType(stepType);
+        setTransition(transitionId);
+        setOriginState(transition.getOriginStateId());
+        setTargetState(transition.getTargetStateId());
+        setStateMachineName(stateMachine.getItemID());
+        setStateMachineVersion(stateMachine.getVersion());
+        setTimeStamp(Event.getGMT());
     }
-    
+
     public Event() { }
-    
-    public int getOriginState() {
-		return mOriginState;
-	}
 
-	public void setOriginState(int originState) {
-		this.mOriginState = originState;
-	}
-
-	public int getTargetState() {
-		return mTargetState;
-	}
-
-	public void setTargetState(int targetState) {
-		this.mTargetState = targetState;
-	}
-
-	public Integer getStateMachineVersion() {
-		return mStateMachineVersion;
-	}
-
-	public void setStateMachineVersion(Integer stateMachineVersion) {
-		this.mStateMachineVersion = stateMachineVersion;
-	}
-
-	public String getStateMachineName() {
-		return mStateMachineName;
-	}
-
-	public void setStateMachineName(String stateMachineName) {
-		this.mStateMachineName = stateMachineName;
-	}
-
-	public void setID( Integer id ) {
+    public void setID( Integer id ) {
         mID = id;
         mName = id==null?null:String.valueOf(id);
     }
-    
-    /**
-     */
-    public void setItemPath( ItemPath itemPath )
-    {
-        mItemPath = itemPath;
-    }
-    
-    public void setItemUUID( String uuid ) throws InvalidItemPathException
-    {
-    	setItemPath(new ItemPath(uuid));
-    }
-    
-    public String getItemUUID() {
-    	return getItemPath().getUUID().toString();
+
+    public void setItemUUID( String uuid ) throws InvalidItemPathException {
+        setItemPath(new ItemPath(uuid));
     }
 
-    public void setAgentUUID( String uuid ) throws InvalidItemPathException
-    {
-    	if (uuid == null || uuid.length() == 0) 
-    		mAgentPath = null;
-    	else if (uuid.contains(":")) {
-    		String[] agentStr = uuid.split(":");
-    		if (agentStr.length!=2)
-    			throw new InvalidItemPathException();
-    		setAgentPath(AgentPath.fromUUIDString(agentStr[0]));
-    		setDelegatePath(AgentPath.fromUUIDString(agentStr[1]));
-    	}
-    	else
-			setAgentPath(AgentPath.fromUUIDString(uuid));
+    public String getItemUUID() {
+        return getItemPath().getUUID().toString();
     }
-    
+
+    public void setAgentUUID( String uuid ) throws InvalidItemPathException {
+        if (uuid == null || uuid.length() == 0) {
+            mAgentPath = null;
+        }
+        else if (uuid.contains(":")) {
+            String[] agentStr = uuid.split(":");
+           
+            if (agentStr.length!=2) throw new InvalidItemPathException();
+
+            setAgentPath(AgentPath.fromUUIDString(agentStr[0]));
+            setDelegatePath(AgentPath.fromUUIDString(agentStr[1]));
+        }
+        else
+            setAgentPath(AgentPath.fromUUIDString(uuid));
+    }
+
     public String getAgentUUID() {
-    	if (mAgentPath != null) {
-    		if (mDelegatePath != null)
-    			return getAgentPath().getUUID().toString()+":"+getDelegatePath().getUUID().toString();
-    		else
-        		return getAgentPath().getUUID().toString();
-    	}
-    	else
-    		return null;
+        if (mAgentPath != null) {
+            if (mDelegatePath != null)
+                return getAgentPath().getUUID().toString()+":"+getDelegatePath().getUUID().toString();
+            else
+                return getAgentPath().getUUID().toString();
+        }
+        else
+            return null;
     }
-    
-    /**
-     * Set the Event Name, in parameter is a String
-     */
+
     @Override
-	public void setName(String name)
-    {
+    public void setName(String name) {
         mName = name;
-        try {
-            mID = Integer.parseInt(name);
-        } catch (NumberFormatException ex) {
-            mID = null;
+
+        if(mID == null) {
+            try {
+                mID = Integer.parseInt(name);
+            }
+            catch (NumberFormatException ex) {}
         }
     }
 
     /**
-     * Set the StepPath of the Event, in parameter is a String
+     *  Return the TimeStamp in a form that will convert nicely to a String: YYYY-MM-DD HH:MI:SS
      */
-    public void setStepName(String name)
-    {
-        mStepName = name;
-    }
-
-    /**
-     * Set the StepPath of the Event, in parameter is a String
-     */
-    public void setStepPath(String path)
-    {
-        mStepPath = path;
-    }
-
-	/**
-	 * Set the StepType of the Event, in parameter is a String
-	 */
-	public void setStepType(String type)
-	{
-		mStepType = type;
-	}
-	
-	/**
-	 * @param viewName the viewName to set
-	 */
-	public void setViewName(String viewName) {
-		this.mViewName = viewName;
-	}
-
-    /**
-     * Set the AgentInfo in the Event, in parameter is an AgentInfo
-     */
-    public void setAgentPath(AgentPath agentPath)
-    {
-        mAgentPath = agentPath;
-    }
-    
-	public void setDelegatePath(AgentPath delegatorPath) {
-		this.mDelegatePath = delegatorPath;
-	}
-
-	public void setAgentRole(String agentRole)
-	{
-		mAgentRole = agentRole;
-	}
-
-    /**
-     * Set the TimeStamp in the Event, in parameter is an GTimeStamp
-     */
-    public void setTimeStamp(GTimeStamp inTimeStamp)
-    {
-        mTimeStamp = inTimeStamp;
-    }
-
-
-    /**
-     * Return the Event's ID
-     */
-    public Integer getID()
-    {
-        return mID;
-    }
-
-    /**
-     */
-    public ItemPath getItemPath()
-    {
-        return mItemPath;
-    }
-
-    /**
-     * Return the Event Name
-     */
-    @Override
-	public String getName()
-    {
-        return mName;
-    }
-
-    /**
-     * Return the StepPath of the Event.
-     */
-    public String getStepName()
-    {
-        return mStepName;
-    }
-
-    /**
-     * Return the StepPath of the Event.
-     */
-    public String getStepPath()
-    {
-        return mStepPath;
-    }
-
-	/**
-	 * Return the StepPath of the Event.
-	 */
-	public String getStepType()
-	{
-		return mStepType;
-	}
-	
-    /**
-	 * @return the mViewName
-	 */
-	public String getViewName() {
-		return mViewName;
-	}
-
-    /**
-     * Return the AgentInfo of the Event.
-     */
-    public AgentPath getAgentPath()
-    {
-        return mAgentPath;
-    }
-
-    public AgentPath getDelegatePath() {
-		return mDelegatePath;
-	}
-	
-	public String getAgentRole()
-    {
-    	return mAgentRole;
-    }
-
-    /**
-     * Return the Event's TimeStamp.
-     */
-    public GTimeStamp getTimeStamp()
-    {
-        return mTimeStamp;
-    }
-
-    /**
-     *  Return the TimeStamp in a form that will
-     *  convert nicely to a String
-     *  YYYY-MM-DD HH:MI:SS
-     */
-    public String getTimeString()
-    {
+    public String getTimeString() {
         return Event.timeToString(mTimeStamp);
+    }
+
+    /**
+     * Converts the timeStamp of the Event to java.util.Date
+     * 
+     * @return java.util.Date
+     */
+    public Date getTimeStampDate() {
+        return new Date(mTimeStamp.mYear-1900, mTimeStamp.mMonth-1, mTimeStamp.mDay, mTimeStamp.mHour, mTimeStamp.mMinute, mTimeStamp.mSecond);
     }
 
     public static String timeToString(GTimeStamp timeStamp) {
@@ -339,80 +170,54 @@ public class Event implements C2KLocalObject
 
     public void setTimeString(String time) throws InvalidDataException
     {
-    	if (time.length() == 19)
-    	    mTimeStamp = new GTimeStamp(
-        					Integer.parseInt(time.substring(0,4)),
-        					Integer.parseInt(time.substring(5,7)),
-        					Integer.parseInt(time.substring(8,10)),
-        					Integer.parseInt(time.substring(11,13)),
-        					Integer.parseInt(time.substring(14,16)),
-    	    				Integer.parseInt(time.substring(17,19)),
-    		   				Calendar.getInstance().get(Calendar.ZONE_OFFSET));
-        else if (time.length() == 14) // support for some sql formats
+        if (time.length() == 19) {
             mTimeStamp = new GTimeStamp(
-                            Integer.parseInt(time.substring(0,4)),
-                            Integer.parseInt(time.substring(4,6)),
-                            Integer.parseInt(time.substring(6,8)),
-                            Integer.parseInt(time.substring(8,10)),
-                            Integer.parseInt(time.substring(10,12)),
-                            Integer.parseInt(time.substring(12,14)),
-                            Calendar.getInstance().get(Calendar.ZONE_OFFSET));
+                    Integer.parseInt(time.substring(0,4)),
+                    Integer.parseInt(time.substring(5,7)),
+                    Integer.parseInt(time.substring(8,10)),
+                    Integer.parseInt(time.substring(11,13)),
+                    Integer.parseInt(time.substring(14,16)),
+                    Integer.parseInt(time.substring(17,19)),
+                    Calendar.getInstance().get(Calendar.ZONE_OFFSET));
+        }
+        else if (time.length() == 14) {
+            // support for some sql formats
+            mTimeStamp = new GTimeStamp(
+                    Integer.parseInt(time.substring(0,4)),
+                    Integer.parseInt(time.substring(4,6)),
+                    Integer.parseInt(time.substring(6,8)),
+                    Integer.parseInt(time.substring(8,10)),
+                    Integer.parseInt(time.substring(10,12)),
+                    Integer.parseInt(time.substring(12,14)),
+                    Calendar.getInstance().get(Calendar.ZONE_OFFSET));
+        }
         else
             throw new InvalidDataException("Unknown time format: "+time);
     }
 
 
-
-   static public GTimeStamp getGMT()
-    {
+    static public GTimeStamp getGMT() {
         java.util.Calendar now = Calendar.getInstance();
 
         return new GTimeStamp( now.get(Calendar.YEAR),
-                               now.get(Calendar.MONTH)+1,
-                               now.get(Calendar.DAY_OF_MONTH),
-                               now.get(Calendar.HOUR_OF_DAY),
-                               now.get(Calendar.MINUTE),
-                               now.get(Calendar.SECOND),
-                               now.get(Calendar.ZONE_OFFSET) );
+                now.get(Calendar.MONTH)+1,
+                now.get(Calendar.DAY_OF_MONTH),
+                now.get(Calendar.HOUR_OF_DAY),
+                now.get(Calendar.MINUTE),
+                now.get(Calendar.SECOND),
+                now.get(Calendar.ZONE_OFFSET) );
     }
 
-	@Override
-	public String getClusterType() {
-		return ClusterStorage.HISTORY;
-	}
-
-    public int getTransition() {
-        return mTransition;
+    @Override
+    public String getClusterType() {
+        return ClusterStorage.HISTORY;
     }
 
-    public void setTransition(int i) {
-        mTransition = i;
-    }
+    public void addOutcomeDetails(Schema schema, String viewName) {
+        setSchemaName(schema.getItemID());
+        setSchemaVersion(schema.getVersion());
 
-	public Integer getSchemaVersion() {
-		return mSchemaVersion;
-	}
-
-	public void setSchemaVersion(Integer schemaVersion) {
-		this.mSchemaVersion = schemaVersion;
-	}
-
-	public String getSchemaName() {
-		return mSchemaName;
-	}
-
-	public void setSchemaName(String schemaName) {
-		this.mSchemaName = schemaName;
-	}
-    
-    public void addOutcomeDetails(Schema schema,
-            String viewName) {
-    	
-		setSchemaName(schema.getItemID());
-		setSchemaVersion(schema.getVersion());
-	    if (viewName == null || viewName.equals(""))
-	    	setViewName("last");
-	    else
-	    	setViewName(viewName);
+        if (viewName == null || viewName.equals("")) setViewName("last");
+        else                                         setViewName(viewName);
     }
 }
