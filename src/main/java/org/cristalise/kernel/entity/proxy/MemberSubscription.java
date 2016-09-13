18822b9ae4de3d8a7131b4e4dedaf8ff8dbadd2e
Zsolt Kovacs <zs.myth@gmail.com>
2016-09-13 21:31:30 +0200

    code reformatted, logs improved

--- src/main/java/org/cristalise/kernel/entity/proxy/MemberSubscription.java
+++ src/main/java/org/cristalise/kernel/entity/proxy/MemberSubscription.java
@@ -29,9 +29,9 @@ import org.cristalise.kernel.utils.Logger;
 
 
 public class MemberSubscription<C extends C2KLocalObject> implements Runnable {
-	public static final String ERROR = "Error";
-	public static final String END = "theEND";
-	
+    public static final String ERROR = "Error";
+    public static final String END = "theEND";
+
     ItemProxy subject;
     String interest;
     // keep the subscriber by weak reference, so it is not kept from the garbage collector if no longer used
@@ -46,7 +46,7 @@ public class MemberSubscription<C extends C2KLocalObject> implements Runnable {
     }
 
     @Override
-	public void run() {
+    public void run() {
         Thread.currentThread().setName("Member Subscription: "+subject.getPath()+":"+interest);
         if (preLoad) loadChildren();
     }
@@ -55,13 +55,14 @@ public class MemberSubscription<C extends C2KLocalObject> implements Runnable {
         C newMember;
         ProxyObserver<C> observer = getObserver();
         if (observer == null) return; //reaped
+
         try {
             // fetch contents of path
             String children = subject.queryData(interest+"/all");
             StringTokenizer tok = new StringTokenizer(children, ",");
             ArrayList<String> newContents = new ArrayList<String>();
-            while (tok.hasMoreTokens())
-                newContents.add(tok.nextToken());
+            
+            while (tok.hasMoreTokens()) newContents.add(tok.nextToken());
 
             // look to see what's new
             for (String newChild: newContents) {
@@ -71,11 +72,13 @@ public class MemberSubscription<C extends C2KLocalObject> implements Runnable {
                     newMember = (C)subject.getObject(interest+"/"+newChild);
                     contents.remove(newChild);
                     observer.add(newMember);
-                } catch (ObjectNotFoundException ex) {
+                }
+                catch (ObjectNotFoundException ex) {
                     observer.control(ERROR, "Listed member "+newChild+" was not found.");
-                } catch (ClassCastException ex) {
-                	Logger.error(ex);
-                	observer.control(ERROR, "Listed member "+newChild+" was the wrong type.");
+                }
+                catch (ClassCastException ex) {
+                    Logger.error(ex);
+                    observer.control(ERROR, "Listed member "+newChild+" was the wrong type.");
                 }
             }
             // report what's left in old contents as deleted
@@ -86,40 +89,43 @@ public class MemberSubscription<C extends C2KLocalObject> implements Runnable {
             contents = newContents;
             //report that we're done
             observer.control(END, null);
-        } catch (Exception ex) {
+        }
+        catch (Exception ex) {
             observer.control(ERROR, "Query on "+interest+" failed with "+ex.getMessage());
         }
     }
 
     public boolean isRelevant(String path) {
-        Logger.msg(7, "Checking relevance of "+path+" to "+interest);
+        Logger.msg(7, "MemberSubscription.isRelevant() - path "+path+" to "+interest);
         return (path.startsWith(interest));
     }
 
     public void update(String path, boolean deleted) {
         ProxyObserver<C> observer = getObserver();
         if (observer == null) return; //reaped
-        Logger.msg(7, "Processing proxy message path "+path +" for "+observer+". Interest: "+interest+" Was Deleted:"+deleted);
+        Logger.msg(7, "MemberSubscription.update() - path "+path +" for "+observer+". Interest: "+interest+" Was Deleted:"+deleted);
+
         if (!path.startsWith(interest)) // doesn't concern us
-           return;
+            return;
 
         if (path.equals(interest)) // refresh contents
-           loadChildren();
+            loadChildren();
         else {
             String name = path.substring(interest.length());
             if (deleted) {
-                Logger.msg(4, "Removing "+path);
+                Logger.msg(4, "MemberSubscription.update() - Removing "+path);
                 contents.remove(name);
                 observer.remove(name);
             }
             else {
                 try {
                     C newMember = (C)subject.getObject(path);
-                    Logger.msg(4, "Adding "+path);
+                    Logger.msg(4, "MemberSubscription.update() - Adding "+path);
                     contents.add(name);
                     observer.add(newMember);
-                } catch (ObjectNotFoundException e) {
-                    Logger.error("Member Subscription: could not load "+path);
+                }
+                catch (ObjectNotFoundException e) {
+                    Logger.error("MemberSubscription: could not load "+path);
                     Logger.error(e);
                 }
             }
@@ -129,9 +135,9 @@ public class MemberSubscription<C extends C2KLocalObject> implements Runnable {
     public void setObserver(ProxyObserver<C> observer) {
         observerReference = new WeakReference<ProxyObserver<C>>(observer);
     }
-    
+
     public void setSubject(ItemProxy subject) {
-    	this.subject = subject;
+        this.subject = subject;
     }
 
     public ProxyObserver<C> getObserver() {
--- src/main/java/org/cristalise/kernel/persistency/RemoteMap.java
+++ src/main/java/org/cristalise/kernel/persistency/RemoteMap.java
@@ -40,377 +40,366 @@ import org.cristalise.kernel.utils.Logger;
 
 /**
  * Maps a storage cluster onto a java.util.Map
- *
- * @author Andrew Branson
- * $Revision: 1.22 $
- * $Date: 2006/03/03 13:52:21 $
- *
- * Copyright (C) 2003 CERN - European Organization for Nuclear Research
- * All rights reserved.
  */
 public class RemoteMap<V extends C2KLocalObject> extends TreeMap<String, V> implements C2KLocalObject  {
 
     private static final long serialVersionUID = -2356840109407419763L;
 
     private int mID=-1;
-	private String mName;
-	protected ItemPath mItemPath;
-	private String mPath = "";
+    private String mName;
+    protected ItemPath mItemPath;
+    private String mPath = "";
     Object keyLock = null;
-	TransactionManager storage;
+    TransactionManager storage;
     Comparator<String> comp;
-    ItemProxy source; // for remote client processes to receive updates, disables puts
+    
+    /**
+     * for remote client processes to receive updates, disables puts. @check activate()
+     */
+    ItemProxy source; 
+
     ProxyObserver<V> listener;
-	Object mLocker; // if this remote map will participate in a transaction
 
-	public RemoteMap(ItemPath itemPath, String path, Object locker) {
-		
-		super(new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				Integer i1 = null, i2 = null;
-				try { 
-					i1 = Integer.valueOf(o1); 
-					i2 = Integer.valueOf(o2);
-					return i1.compareTo(i2);
-					} catch (NumberFormatException ex) { }
-				return o1.compareTo(o2);
-			}
-		});
-		
-		mItemPath = itemPath;
-		mLocker = locker;
+    /**
+     * if this remote map will participate in a transaction
+     */
+    Object mLocker;
 
-		// split the path into path/name
-		int lastSlash = path.lastIndexOf("/");
-		mName = path.substring(lastSlash+1);
-		if (lastSlash>0) mPath = path.substring(0,lastSlash);
+    public RemoteMap(ItemPath itemPath, String path, Object locker) {
 
-		// see if the name is also a suitable id
-		try {
-			mID = Integer.parseInt(mName);
-		} catch (NumberFormatException e) {}
-		storage = Gateway.getStorage();
-		
-	}
-	
-	public void activate() {
-    	listener = new ProxyObserver<V>() {
-	        @Override
-			public void add(V obj) {
-	            synchronized (this) {
-	                putLocal(obj.getName(), obj);
-	            }
-	        }
-	
-	        @Override
-			public void remove(String id) {
-	            synchronized (this) {
-	                removeLocal(id);
-	            }
-	        }
-	
-			@Override
-			public void control(String control, String msg) { }
-	    };
+        super(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                Integer i1 = null, i2 = null;
+                try { 
+                    i1 = Integer.valueOf(o1); 
+                    i2 = Integer.valueOf(o2);
+                    return i1.compareTo(i2);
+                }
+                catch (NumberFormatException ex) { }
 
+                return o1.compareTo(o2);
+            }
+        });
 
-    	try {
-    		source = Gateway.getProxyManager().getProxy(mItemPath);
-    		source.subscribe(new MemberSubscription<V>(listener, mPath+mName, false));
-    	} catch (Exception ex) {
-    		Logger.error("Error subscribing to remote map. Changes will not be received");
-    		Logger.error(ex);
-    	}
-	}
-	
-	public void deactivate() {
-		if (source != null) source.unsubscribe(listener);
-	}
-	
-	@Override
-	public void finalize() {
-		deactivate();
-		Gateway.getStorage().clearCache(mItemPath, mPath+mName);
-	}
+        mItemPath = itemPath;
+        mLocker = locker;
 
-	protected void loadKeys() {
-		if (keyLock != null) return;
-		clear();
-		keyLock = new Object();
-        synchronized(this) {
-    		String[] keys;
-			try {
-				keys = storage.getClusterContents(mItemPath, mPath+mName);
-				for (String key : keys) super.put(key, null);
-			} catch (PersistencyException e) {
-				Logger.error(e);
-			}
+        // split the path into path/name
+        int lastSlash = path.lastIndexOf("/");
+        mName = path.substring(lastSlash+1);
+        if (lastSlash>0) mPath = path.substring(0,lastSlash);
 
+        // see if the name is also a suitable id
+        try {
+            mID = Integer.parseInt(mName);
         }
-	}
+        catch (NumberFormatException e) {}
 
-    public synchronized int getLastId() {
-    	loadKeys();
-    	if (size() == 0) return -1;
-    	try {
-    		return Integer.parseInt(lastKey());
-    	} catch (NumberFormatException ex) {
-    		return -1;
-    	}
+        storage = Gateway.getStorage();
     }
 
+    public void activate() {
+        listener = new ProxyObserver<V>() {
+            @Override
+            public void add(V obj) {
+                synchronized (this) {
+                    putLocal(obj.getName(), obj);
+                }
+            }
 
-	// c2kLocalObject methods
-	public void setID(int id) { mID = id; }
+            @Override
+            public void remove(String id) {
+                synchronized (this) {
+                    removeLocal(id);
+                }
+            }
 
-	public int getID() { return mID; }
-
-	@Override
-	public void setName(String name) { mName = name; }
-
-	@Override
-	public String getName() { return mName; }
-
-	/**
-	 * Cannot be stored
-	 */
-	@Override
-	public String getClusterType() {
-		return null;
-	}
-	/**
-	 * @see java.util.Map#clear()
-	 */
-	@Override
-	public synchronized void clear() {
-		synchronized (this) {
-			super.clear();
-		}
-		keyLock = null;
-	}
+            @Override
+            public void control(String control, String msg) { }
+        };
 
 
-
-	/**
-	 * @see java.util.Map#containsKey(Object)
-	 */
-	@Override
-	public synchronized boolean containsKey(Object key) {
-		if (keyLock == null) loadKeys();
-		return super.containsKey(key);
-	}
-
-	/**
-	 * This must retrieve all the values until a match is made.
-	 * Very expensive, but if you must, you must.
-	 * @see java.util.Map#containsValue(Object)
-	 */
-	@Override
-	public synchronized boolean containsValue(Object value) {
-		loadKeys();
-        synchronized(this) {
-    		for (String key: keySet()) {
-   				if (get(key).equals(value)) return true;
-    		}
+        try {
+            source = Gateway.getProxyManager().getProxy(mItemPath);
+            source.subscribe(new MemberSubscription<V>(listener, mPath+mName, false));
         }
-		return false;
-	}
+        catch (Exception ex) {
+            Logger.error("Error subscribing to remote map. Changes will NOT be received");
+            Logger.error(ex);
+        }
+    }
 
+    public void deactivate() {
+        if (source != null) source.unsubscribe(listener);
+    }
 
-	/**
-	 * @see java.util.Map#get(Object)
-	 */
     @Override
-	public synchronized V get(Object objKey) {
-		loadKeys();
-		String key;
-		if (objKey instanceof Integer)
-			key = ((Integer)objKey).toString();
-		else if (objKey instanceof String)
-			key = (String)objKey;
-		else 
-			return null;
-		
+    public void finalize() {
+        deactivate();
+        Gateway.getStorage().clearCache(mItemPath, mPath+mName);
+    }
+
+    protected void loadKeys() {
+        if (keyLock != null) return;
+        clear();
+        keyLock = new Object();
+        
         synchronized(this) {
-    		try {
-    			V value = super.get(key);
-    			if (value == null) {
-    				value = (V)storage.get(mItemPath, mPath+mName+"/"+key, mLocker);
-    				super.put(key, value);
-    			}
-    			return value;
-    		} catch (PersistencyException e) {
-    				Logger.error(e);
-    		} catch (ObjectNotFoundException e) {
+            String[] keys;
+            try {
+                keys = storage.getClusterContents(mItemPath, mPath+mName);
+                for (String key : keys) super.put(key, null);
+            }
+            catch (PersistencyException e) {
                 Logger.error(e);
             }
         }
-		return null;
-	}
+    }
 
-	/**
-	 * @see java.util.Map#isEmpty()
-	 */
-	@Override
-	public synchronized boolean isEmpty() {
-		loadKeys();
-		return super.isEmpty();
-	}
+    public synchronized int getLastId() {
+        loadKeys();
+        if (size() == 0) return -1;
+        try {
+            return Integer.parseInt(lastKey());
+        }
+        catch (NumberFormatException ex) {
+            return -1;
+        }
+    }
 
-	/**
-	 * @see java.util.Map#keySet()
-	 */
-	@Override
-	public synchronized Set<String> keySet() {
-		loadKeys();
-		return super.keySet();
-	}
+    public void setID(int id) { mID = id; }
 
-	/**
-	 * Inserts the given object into the storage
-	 * the key is ignored - it can be fetched from the value.
-	 * @see java.util.Map#put(Object, Object)
-	 */
-	@Override
-	public synchronized V put(String key, V value) {
-		if (source != null) throw new UnsupportedOperationException("Cannot use an activated RemoteMap to write to storage.");
-		try {
+    public int getID() { return mID; }
+
+    @Override
+    public void setName(String name) { mName = name; }
+
+    @Override
+    public String getName() { return mName; }
+
+    /**
+     * Returns null so it cannot be stored
+     */
+    @Override
+    public String getClusterType() {
+        return null;
+    }
+
+    @Override
+    public synchronized void clear() {
+        synchronized (this) {
+            super.clear();
+        }
+        keyLock = null;
+    }
+
+    @Override
+    public synchronized boolean containsKey(Object key) {
+        if (keyLock == null) loadKeys();
+        return super.containsKey(key);
+    }
+
+    /**
+     * This must retrieve all the values until a match is made.
+     * Very expensive, but if you must, you must.
+     * @see java.util.Map#containsValue(Object)
+     */
+    @Override
+    public synchronized boolean containsValue(Object value) {
+        loadKeys();
+        synchronized(this) {
+            for (String key: keySet()) {
+                if (get(key).equals(value)) return true;
+            }
+        }
+        return false;
+    }
+
+
+    @Override
+    public synchronized V get(Object objKey) {
+        loadKeys();
+        String key;
+        
+        if (objKey instanceof Integer)     key = ((Integer)objKey).toString();
+        else if (objKey instanceof String) key = (String)objKey;
+        else                               return null;
+
+        synchronized(this) {
+            try {
+                V value = super.get(key);
+                if (value == null) {
+                    value = (V)storage.get(mItemPath, mPath+mName+"/"+key, mLocker);
+                    super.put(key, value);
+                }
+                return value;
+            }
+            catch (PersistencyException e) {
+                Logger.error(e);
+            }
+            catch (ObjectNotFoundException e) {
+                Logger.error(e);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public synchronized boolean isEmpty() {
+        loadKeys();
+        return super.isEmpty();
+    }
+
+    /**
+     * @see java.util.Map#keySet()
+     */
+    @Override
+    public synchronized Set<String> keySet() {
+        loadKeys();
+        return super.keySet();
+    }
+
+    /**
+     * Inserts the given object into the storage
+     * the key is ignored - it can be fetched from the value.
+     * @see java.util.Map#put(Object, Object)
+     */
+    @Override
+    public synchronized V put(String key, V value) {
+        if (source != null) throw new UnsupportedOperationException("Cannot use an activated RemoteMap to write to storage.");
+        try {
             synchronized(this) {
                 storage.put(mItemPath, value, mLocker);
                 return putLocal(key, value);
             }
-		} catch (PersistencyException e) {
-			Logger.error(e);
-			return null;
-		}
-	}
-	
-	protected synchronized V putLocal(String key, V value) {
-		return super.put(key, value);
-	}
+        }
+        catch (PersistencyException e) {
+            Logger.error(e);
+            return null;
+        }
+    }
 
-	/**
-	 * @see java.util.Map#remove(Object)
-	 */
-	@Override
-	public synchronized V remove(Object key) {
-		if (source != null) throw new UnsupportedOperationException("Cannot use an activated RemoteMap to write to storage.");
-		loadKeys();
-		if (containsKey(key)) try {
+    protected synchronized V putLocal(String key, V value) {
+        return super.put(key, value);
+    }
+
+    /**
+     * @see java.util.Map#remove(Object)
+     */
+    @Override
+    public synchronized V remove(Object key) {
+        if (source != null) throw new UnsupportedOperationException("Cannot use an activated RemoteMap to write to storage.");
+        loadKeys();
+        if (containsKey(key)) try {
             synchronized(keyLock) {
-			    storage.remove(mItemPath, mPath+mName+"/"+key, mLocker);
-			    return removeLocal(key);
+                storage.remove(mItemPath, mPath+mName+"/"+key, mLocker);
+                return removeLocal(key);
             }
-		} catch (PersistencyException e) {
-			Logger.error(e);
-		}
-		return null;
-	}
-	
-	protected synchronized V removeLocal(Object key) {
-		return super.remove(key);
-	}
+        }
+        catch (PersistencyException e) {
+            Logger.error(e);
+        }
+        return null;
+    }
 
-	/**
-	 * @see java.util.Map#size()
-	 */
-	@Override
-	public synchronized int size() {
-		loadKeys();
-		return super.size();
-	}
+    protected synchronized V removeLocal(Object key) {
+        return super.remove(key);
+    }
 
-	/**
-	 * @see java.util.Map#values()
-	 */
-	@Override
-	public synchronized Collection<V> values() {
-		return new RemoteSet<V>(this);
-	}
+    /**
+     * @see java.util.Map#size()
+     */
+    @Override
+    public synchronized int size() {
+        loadKeys();
+        return super.size();
+    }
 
-	/**
-	 * Basic implementation of Set and Collection to bridge to the Iterator
-	 * Disallows all writes.
-	 */
+    /**
+     * @see java.util.Map#values()
+     */
+    @Override
+    public synchronized Collection<V> values() {
+        return new RemoteSet<V>(this);
+    }
 
-	private class RemoteSet<E extends C2KLocalObject> extends AbstractSet<E> {
-		RemoteMap<E> mParent;
+    /**
+     * Basic implementation of Set and Collection to bridge to the Iterator
+     * Disallows all writes.
+     */
+    private class RemoteSet<E extends C2KLocalObject> extends AbstractSet<E> {
+        RemoteMap<E> mParent;
 
-		public RemoteSet(RemoteMap<E> parent) {
-			mParent = parent;
-		}
+        public RemoteSet(RemoteMap<E> parent) {
+            mParent = parent;
+        }
 
-		// no modifications allowed
-		@Override
-		public boolean add(E o) {
-			throw new UnsupportedOperationException();
-		}
-		@Override
-		public boolean addAll(Collection<? extends E> c) {
-			throw new UnsupportedOperationException();
-		}
-		@Override
-		public void clear() {
-			throw new UnsupportedOperationException();
-		}
-		@Override
-		public boolean remove(Object o) {
-			throw new UnsupportedOperationException();
-		}
-		@Override
-		public boolean removeAll(Collection<?> c) {
-			throw new UnsupportedOperationException();
-		}
-		@Override
-		public boolean retainAll(Collection<?> c) {
-			throw new UnsupportedOperationException();
-		}
+        // no modifications allowed
+        @Override
+        public boolean add(E o) {
+            throw new UnsupportedOperationException();
+        }
+        @Override
+        public boolean addAll(Collection<? extends E> c) {
+            throw new UnsupportedOperationException();
+        }
+        @Override
+        public void clear() {
+            throw new UnsupportedOperationException();
+        }
+        @Override
+        public boolean remove(Object o) {
+            throw new UnsupportedOperationException();
+        }
+        @Override
+        public boolean removeAll(Collection<?> c) {
+            throw new UnsupportedOperationException();
+        }
+        @Override
+        public boolean retainAll(Collection<?> c) {
+            throw new UnsupportedOperationException();
+        }
 
-		@Override
-		public Iterator<E> iterator() {
-			return new RemoteIterator<E>(mParent);
-		}
+        @Override
+        public Iterator<E> iterator() {
+            return new RemoteIterator<E>(mParent);
+        }
 
-		@Override
-		public int size() {
-			return mParent.size();
-		}
+        @Override
+        public int size() {
+            return mParent.size();
+        }
 
-	}
-	/**
-	 * Iterator view on RemoteMap data. Doesn't preload anything.
-	 * REVISIT: Will go strange if the RemoteMap is modified. Detect this and throw ConcurrentMod ex
-	 */
-	private class RemoteIterator<C extends C2KLocalObject> implements Iterator<C> {
-		RemoteMap<C> mParent;
-		Iterator<String> iter;
-		String currentKey;
+    }
+    /**
+     * Iterator view on RemoteMap data. Doesn't preload anything.
+     * REVISIT: Will go strange if the RemoteMap is modified. Detect this and throw ConcurrentMod ex
+     */
+    private class RemoteIterator<C extends C2KLocalObject> implements Iterator<C> {
+        RemoteMap<C> mParent;
+        Iterator<String> iter;
+        String currentKey;
 
-		public RemoteIterator(RemoteMap<C> parent) {
-			mParent = parent;
-			iter = mParent.keySet().iterator();
-		}
+        public RemoteIterator(RemoteMap<C> parent) {
+            mParent = parent;
+            iter = mParent.keySet().iterator();
+        }
 
-		@Override
-		public boolean hasNext() {
-			return iter.hasNext();
-		}
+        @Override
+        public boolean hasNext() {
+            return iter.hasNext();
+        }
 
-		@Override
-		public C next() {
-			currentKey = iter.next();
-			return mParent.get(currentKey);
-		}
+        @Override
+        public C next() {
+            currentKey = iter.next();
+            return mParent.get(currentKey);
+        }
 
-		@Override
-		public void remove() {
-			throw new UnsupportedOperationException();
-		}
-
-	}
-
-
-
-
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
 }
