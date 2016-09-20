da478f5360f4ac2e853a100c334c30cd74137743
Zsolt Kovacs <zs.myth@gmail.com>
2016-09-20 19:59:39 +0200

    complete reformat and refactoring to support different bootstrap uses cases including testing

--- src/main/java/org/cristalise/kernel/entity/imports/ImportItem.java
+++ src/main/java/org/cristalise/kernel/entity/imports/ImportItem.java
@@ -20,8 +20,8 @@
  */
 package org.cristalise.kernel.entity.imports;
 
-
 import static org.cristalise.kernel.property.BuiltInItemProperties.NAME;
+import static org.cristalise.kernel.property.BuiltInItemProperties.CREATOR;
 
 import java.util.ArrayList;
 
@@ -39,6 +39,8 @@ import org.cristalise.kernel.entity.TraceableEntity;
 import org.cristalise.kernel.events.Event;
 import org.cristalise.kernel.events.History;
 import org.cristalise.kernel.lifecycle.CompositeActivityDef;
+import org.cristalise.kernel.lifecycle.instance.CompositeActivity;
+import org.cristalise.kernel.lifecycle.instance.Workflow;
 import org.cristalise.kernel.lifecycle.instance.predefined.PredefinedStep;
 import org.cristalise.kernel.lookup.AgentPath;
 import org.cristalise.kernel.lookup.DomainPath;
@@ -58,250 +60,269 @@ import org.cristalise.kernel.utils.Logger;
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.XMLUnit;
 
+import lombok.Getter;
+import lombok.Setter;
 
 /**
- * Complete Structure for new item
- *
- * @version $Revision: 1.8 $ $Date: 2006/03/03 13:52:21 $
+ * Complete Structure for new Item created by different bootstrap uses cases including testing
  */
-
+@Getter @Setter
 public class ImportItem extends ModuleImport {
 
-    protected String initialPath;
-    protected String workflow;
+    protected String  initialPath;
+    protected String  workflow;
     protected Integer workflowVer;
-    protected ArrayList<Property> properties  = new ArrayList<Property>();
-    protected ArrayList<ImportAggregation> aggregationList = new ArrayList<ImportAggregation>();
-    protected ArrayList<ImportDependency> dependencyList = new ArrayList<ImportDependency>();
-    protected ArrayList<ImportOutcome> outcomes = new ArrayList<ImportOutcome>();
 
-    public ImportItem() {
-    }
+    protected ArrayList<Property> properties = new ArrayList<Property>();
+
+    protected ArrayList<ImportAggregation> aggregationList = new ArrayList<ImportAggregation>();
+    protected ArrayList<ImportDependency>  dependencyList  = new ArrayList<ImportDependency>();
+    protected ArrayList<ImportOutcome>     outcomes        = new ArrayList<ImportOutcome>();
+
+    /**
+     * Alternative way to provide workflow definition.
+     * It is not marshallable by castor, therefore cannot be set in module.xml
+     */
+    protected CompositeActivityDef compActDef;
+
+    /**
+     * Alternative way to provide workflow.
+     * It is not marshallable by castor, therefore cannot be set in module.xml
+     */
+    protected Workflow wf;
+
+    //TODO: Check if these 2 flags should go to class ModuleImport
+    protected boolean isNewItem = true;
+    protected boolean isDOMPathExists = true; //avoids multiple call to domainPath.exists()
+
+    public ImportItem() {}
 
     public ImportItem(String ns, String name, String initialPath, ItemPath itemPath, String wf, int wfVer) {
-        this();
         setNamespace(ns);
         setName(name);
         setItemPath(itemPath);
         setInitialPath(initialPath);
         setWorkflow(wf);
         setWorkflowVer(wfVer);
+
+        compActDef = null;
+        wf = null;
     }
-    
+
+    /**
+     * Try to find ItemPath if already exists. If not create new one.
+     */
     @Override
     public ItemPath getItemPath() {
-        if (itemPath == null) { // try to find item if it already exists
-        	DomainPath existingItem = new DomainPath(initialPath+"/"+name);
-        	if (existingItem.exists()) {
-        		try {
-        			itemPath = existingItem.getItemPath();
-        		} catch (ObjectNotFoundException ex) { }
-        	}
+        if (itemPath == null) { 
+            DomainPath existingItem = new DomainPath(initialPath + "/" + name);
+            if (existingItem.exists()) {
+                try {
+                    itemPath = existingItem.getItemPath();
+                }
+                catch (ObjectNotFoundException ex) {}
+            }
         }
         if (itemPath == null) itemPath = new ItemPath();
+
         return itemPath;
     }
 
-	@Override
-	public void setNamespace(String ns) {
-		super.setNamespace(ns);
-		if (initialPath == null) initialPath = "/desc/"+ns;
-	}
+    @Override
+    public void setNamespace(String ns) {
+        super.setNamespace(ns);
+        if (initialPath == null) initialPath = "/desc/" + ns;
+    }
 
-	@Override
-	public void setName(String name) {
-		super.setName(name);
-	}
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
 
-	@Override
-	public Path create(AgentPath agentPath, boolean reset) throws ObjectCannotBeUpdated, ObjectNotFoundException, CannotManageException, ObjectAlreadyExistsException, InvalidCollectionModification {
-        DomainPath domPath = new DomainPath(new DomainPath(initialPath), name);
-        if (domPath.exists()) {
-        	ItemPath domItem = domPath.getItemPath();
-        	if (!getItemPath().equals(domItem))
-        		throw new CannotManageException("Item "+domPath+" was found with the wrong itemPath ("+domPath.getItemPath()+" vs "+getItemPath()+")");
-        }
-        
+    /**
+     * 
+     * @return
+     * @throws ObjectNotFoundException
+     * @throws CannotManageException
+     * @throws ObjectAlreadyExistsException
+     * @throws ObjectCannotBeUpdated
+     */
+    private TraceableEntity getTraceableEntitiy()
+            throws ObjectNotFoundException, CannotManageException, ObjectAlreadyExistsException, ObjectCannotBeUpdated
+    {
         TraceableEntity newItem;
-        if (getItemPath().exists()) {
-        	Logger.msg(1, "ImportItem.create() - Verifying module item "+domPath+" at "+getItemPath());
-        	newItem = Gateway.getCorbaServer().getItem(getItemPath());
-        } 
+        ItemPath ip = getItemPath();
+
+        if (ip.exists()) {
+            Logger.msg(1, "ImportItem.getTraceableEntitiy() - Verifying module item "+domainPath+" at "+ip);
+            newItem = Gateway.getCorbaServer().getItem(getItemPath());
+            isNewItem = false;
+        }
         else {
-        	Logger.msg("ImportItem.create() - Creating module item "+getItemPath()+" at "+domPath);
-	        newItem = Gateway.getCorbaServer().createItem(getItemPath());
-	        Gateway.getLookupManager().add(getItemPath());
+            Logger.msg("ImportItem.getTraceableEntitiy() - Creating module item "+ip+" at "+domainPath);
+            newItem = Gateway.getCorbaServer().createItem(ip);
+            Gateway.getLookupManager().add(ip);
         }
-        
-        // set the name property
-        properties.add(new Property(NAME, name, true));
+        return newItem;
+    }
 
-        // find workflow def
-        CompositeActivityDef compact;
-    	// default workflow version is 0 if not given
-    	int usedWfVer;
-    	if (workflowVer == null) usedWfVer = 0;
-    	else usedWfVer = workflowVer.intValue();
-        try {
-        	compact = (CompositeActivityDef)LocalObjectLoader.getActDef(workflow, usedWfVer);
-        } catch (ObjectNotFoundException ex) {
-        	throw new CannotManageException("Could not find workflow "+workflow+"v"+usedWfVer+" for item "+domPath);
-        } catch (InvalidDataException e) {
-        	throw new CannotManageException("Workflow def "+workflow+" v"+usedWfVer+" for item "+domPath+" was not valid");
-		}
-        
-        // create collections
-        CollectionArrayList colls = new CollectionArrayList();
-        for (ImportDependency element: dependencyList) {
-           	Dependency newDep = element.create();
-           	colls.put(newDep);
-        }
+    /**
+     * 
+     */
+    @Override
+    public Path create(AgentPath agentPath, boolean reset)
+            throws InvalidDataException, ObjectCannotBeUpdated, ObjectNotFoundException,
+                   CannotManageException, ObjectAlreadyExistsException, InvalidCollectionModification, PersistencyException
+    {
+        domainPath = new DomainPath(new DomainPath(initialPath), name);
 
-        for (ImportAggregation element : aggregationList) {
-           	Aggregation newAgg = element.create();           	
-           	colls.put(newAgg);
+        if (domainPath.exists()) {
+            ItemPath domItem = domainPath.getItemPath();
+            if (!getItemPath().equals(domItem)) {
+                throw new CannotManageException("Item "+domainPath+" was found with the wrong itemPath ("+domainPath.getItemPath()+" vs "+getItemPath()+")");
+            }
         }
- 
-    	// (re)initialise the new item with properties, workflow and collections
+        else isDOMPathExists = false;
+
+        TraceableEntity newItem = getTraceableEntitiy();
+
+        // (re)initialise the new item with properties, workflow and collections
         try {
-            newItem.initialise(
-            		agentPath.getSystemKey(),
-                    Gateway.getMarshaller().marshall(new PropertyArrayList(properties)),
-                    Gateway.getMarshaller().marshall(compact.instantiate()),
-                    Gateway.getMarshaller().marshall(colls));
-        } catch (Exception ex) {
-            Logger.error("Error initialising new item "+ns+"/"+name );
+            newItem.initialise( agentPath.getSystemKey(),
+                                Gateway.getMarshaller().marshall(createItemProperties()),
+                                Gateway.getMarshaller().marshall(createCompositeActivity()), 
+                                Gateway.getMarshaller().marshall(createCollections()));
+        }
+        catch (Exception ex) {
+            Logger.error("Error initialising new item " + ns + "/" + name);
             Logger.error(ex);
-            throw new CannotManageException("Problem initialising new item. See server log.");
+
+            if (isNewItem) Gateway.getLookupManager().delete(itemPath);
+
+            throw new CannotManageException("Problem initialising new item. See server log:" + ex.getMessage());
         }
+
+        History hist = new History(getItemPath(), null);
 
         // import outcomes
-        XMLUnit.setIgnoreWhitespace(true);
-        XMLUnit.setIgnoreComments(true);
-        History hist = new History(getItemPath(), null);
         for (ImportOutcome thisOutcome : outcomes) {
-	        String outcomeData = thisOutcome.getData(ns);
-	        
-	        //load schema and state machine
-	        Schema schema;
-	        try { 
-	        	schema = LocalObjectLoader.getSchema(thisOutcome.schema, thisOutcome.version);
-	        } catch (InvalidDataException ex) {
-	        	throw new ObjectCannotBeUpdated("Could not load schema "+thisOutcome.schema+" v"+thisOutcome.version);
-	        }
-	        
-	        //parse new outcome and validate
-        	Outcome newOutcome;
-        	try {
-        		newOutcome = new Outcome(-1, outcomeData, schema);
-            	String errors = newOutcome.validate();
-            	if (errors != null && errors.length() > 0) 
-            		throw new ObjectCannotBeUpdated("Invalid XML in view "+thisOutcome.schema+"/"+thisOutcome.viewname+" in "+ns+"/"+name);
-        	} catch (InvalidDataException ex) {
-        		throw new ObjectCannotBeUpdated("XML is not valid in view "+thisOutcome.schema+"/"+thisOutcome.viewname+" in "+ns+"/"+name);
-        	}
-        	
-        	
-        	Viewpoint impView;
-        	try {
-        		impView = (Viewpoint)Gateway.getStorage().get(getItemPath(), ClusterStorage.VIEWPOINT+"/"+thisOutcome.schema+"/"+thisOutcome.viewname, null);
+            String outcomeData = thisOutcome.getData(ns);
+
+            // load schema and state machine
+            Schema schema = LocalObjectLoader.getSchema(thisOutcome.schema, thisOutcome.version);
+
+            // parse new outcome and validate
+            Outcome newOutcome = new Outcome(-1, outcomeData, schema);
+            String errors = newOutcome.validate();
+            
+            if (errors != null && errors.length() > 0) {
+                Logger.error("ImportItem.create() - Invalid XML errors:" + errors);
+                throw new ObjectCannotBeUpdated("Invalid XML for " + thisOutcome.schema + "/" + thisOutcome.viewname + " in " + ns + "/" + name);
+            }
+
+            Viewpoint impView;
+            try {
+                XMLUnit.setIgnoreWhitespace(true);
+                XMLUnit.setIgnoreComments(true);
+
+                impView = (Viewpoint) Gateway.getStorage().get(getItemPath(), ClusterStorage.VIEWPOINT + "/" + thisOutcome.schema + "/" + thisOutcome.viewname, null);
 
                 Diff xmlDiff = new Diff(newOutcome.getDOM(), impView.getOutcome().getDOM());
+                
                 if (xmlDiff.identical()) {
-                    Logger.msg(5, "NewItem.create() - View "+thisOutcome.schema+"/"+thisOutcome.viewname+" in "+ns+"/"+name+" identical, no update required");
+                    Logger.msg(5, "ImportItem.create() - View "+thisOutcome.schema+"/"+thisOutcome.viewname+" in "+ns+"/"+name+" identical, no update required");
                     continue;
                 }
                 else {
-                	Logger.msg("NewItem.create() - Difference found in view "+thisOutcome.schema+"/"+thisOutcome.viewname+" in "+ns+"/"+name+": "+xmlDiff.toString());
-                	if (!reset && !impView.getEvent().getStepPath().equals("Import")) {
-                		Logger.msg("Last edit was not done by import, and reset not requested. Not overwriting.");
-                		continue;
-                	}
+                    Logger.msg("ImportItem.create() - Difference found in view "+thisOutcome.schema+"/"+thisOutcome.viewname+" in "+ns+"/"+name+": "+xmlDiff.toString());
+
+                    if (!reset && !impView.getEvent().getStepPath().equals("Import")) {
+                        Logger.msg("ImportItem.create() - Last edit was not done by import, and reset not requested. Not overwriting.");
+                        continue;
+                    }
                 }
-        	} catch (ObjectNotFoundException ex) { 
-        		Logger.msg("View "+thisOutcome.schema+"/"+thisOutcome.viewname+" not found in "+ns+"/"+name+". Creating.");
-        		impView = new Viewpoint(getItemPath(), schema, thisOutcome.viewname, -1);
-        	} catch (PersistencyException e) {
-        		throw new ObjectCannotBeUpdated("Could not check data for view "+thisOutcome.schema+"/"+thisOutcome.viewname+" in "+ns+"/"+name);
-			} catch (InvalidDataException e) {
-				throw new ObjectCannotBeUpdated("Could not check previous event for view "+thisOutcome.schema+"/"+thisOutcome.viewname+" in "+ns+"/"+name);
-			}
-        	
-        	// write new view/outcome/event
+            }
+            catch (ObjectNotFoundException ex) {
+                Logger.msg("ImportItem.create() - View "+thisOutcome.schema+"/"+thisOutcome.viewname+" not found in "+ns+"/"+name+". Creating.");
+                impView = new Viewpoint(getItemPath(), schema, thisOutcome.viewname, -1);
+            }
+
+            // write new view/outcome/event
             Event newEvent = hist.addEvent(agentPath, null, "Admin", "Import", "Import", "Import", schema, Bootstrap.getPredefSM(), PredefinedStep.DONE, thisOutcome.viewname);
             newOutcome.setID(newEvent.getID());
             impView.setEventId(newEvent.getID());
-            try {
-				Gateway.getStorage().put(getItemPath(), newOutcome, null);
-				Gateway.getStorage().put(getItemPath(), impView, null);
-			} catch (PersistencyException e) {
-				throw new ObjectCannotBeUpdated("Could not store data for view "+thisOutcome.schema+"/"+thisOutcome.viewname+" in "+ns+"/"+name);
-			}
-		}
-        
-        // register domain path (before collections in case of recursive collections)
-        if (!domPath.exists()) {
-        	domPath.setItemPath(getItemPath());
-        	Gateway.getLookupManager().add(domPath);
+
+            Gateway.getStorage().put(getItemPath(), newOutcome, null);
+            Gateway.getStorage().put(getItemPath(), impView, null);
         }
-        
-        return domPath;
+
+        // register domain path (before collections in case of recursive collections)
+        if (!isDOMPathExists) {
+            domainPath.setItemPath(getItemPath());
+            Gateway.getLookupManager().add(domainPath);
+        }
+
+        return domainPath;
     }
-	
-    public String getInitialPath() {
-		return initialPath;
-	}
 
-	public void setInitialPath(String initialPath) {
-		this.initialPath = initialPath;
-	}
+    /**
+     * 
+     * @return
+     */
+    protected PropertyArrayList createItemProperties() {
+        properties.add(new Property(NAME, name, true));
+        properties.add(new Property(CREATOR, "bootstrap", true));
 
-	public String getWorkflow() {
-		return workflow;
-	}
+        return new PropertyArrayList(properties);
+    }
 
-	public void setWorkflow(String workflow) {
-		this.workflow = workflow;
-	}
+    /**
+     * This method enables to use ImportItem in different bootstrap uses cases (e.g. testing)
+     * 
+     * @return the the domain workflow of the Item
+     * @throws ObjectNotFoundException
+     * @throws InvalidDataException
+     */
+    protected CompositeActivity createCompositeActivity() throws ObjectNotFoundException, InvalidDataException {
+        if (wf != null) {
+            return (CompositeActivity) wf.search("workflow/domain");
+        }
+        else {
+            if (compActDef == null) {
+                // default workflow version is 0 if not given
+                if (workflow != null && workflow.length() != 0) {
+                    compActDef = (CompositeActivityDef) LocalObjectLoader.getActDef(workflow, workflowVer == null ? 0 : workflowVer);
+                    return (CompositeActivity) compActDef.instantiate();
+                }
+                else {
+                    Logger.warning("ImportItem.createCompositeActivity() - NO Workflow was set for domainPath:"+domainPath);
+                }
+            }
+        }
+        return null;
+    }
 
-	public Integer getWorkflowVer() {
-		return workflowVer;
-	}
+    /**
+     * 
+     * @return
+     * @throws InvalidCollectionModification
+     * @throws ObjectNotFoundException
+     * @throws ObjectAlreadyExistsException
+     */
+    protected CollectionArrayList createCollections() 
+            throws InvalidCollectionModification, ObjectNotFoundException, ObjectAlreadyExistsException
+    {
+        CollectionArrayList colls = new CollectionArrayList();
+        
+        for (ImportDependency element : dependencyList) {
+            Dependency newDep = element.create();
+            colls.put(newDep);
+        }
 
-	public void setWorkflowVer(Integer workflowVer) {
-		this.workflowVer = workflowVer;
-	}
-
-	public ArrayList<Property> getProperties() {
-		return properties;
-	}
-
-	public void setProperties(ArrayList<Property> properties) {
-		this.properties = properties;
-	}
-
-	public ArrayList<ImportAggregation> getAggregationList() {
-		return aggregationList;
-	}
-
-	public void setAggregationList(ArrayList<ImportAggregation> aggregationList) {
-		this.aggregationList = aggregationList;
-	}
-
-	public ArrayList<ImportDependency> getDependencyList() {
-		return dependencyList;
-	}
-
-	public void setDependencyList(ArrayList<ImportDependency> dependencyList) {
-		this.dependencyList = dependencyList;
-	}
-
-	public ArrayList<ImportOutcome> getOutcomes() {
-		return outcomes;
-	}
-
-	public void setOutcomes(ArrayList<ImportOutcome> outcomes) {
-		this.outcomes = outcomes;
-	}
-
+        for (ImportAggregation element : aggregationList) {
+            Aggregation newAgg = element.create();
+            colls.put(newAgg);
+        }
+        return colls;
+    }
 }
