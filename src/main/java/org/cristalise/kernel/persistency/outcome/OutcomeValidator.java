5e22650440e7d00e991d661c873a1184dfe67e6b
Zsolt Kovacs <zs.myth@gmail.com>
2016-12-28 14:01:41 +0100

    mainly code reformat. created new validate() to merge duplicate code

--- src/main/java/org/cristalise/kernel/persistency/outcome/OutcomeValidator.java
+++ src/main/java/org/cristalise/kernel/persistency/outcome/OutcomeValidator.java
@@ -20,14 +20,17 @@
  */
 package org.cristalise.kernel.persistency.outcome;
 
+import java.io.IOException;
 import java.io.StringReader;
 
 import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
+import org.apache.commons.lang3.StringUtils;
 import org.cristalise.kernel.common.InvalidDataException;
 import org.cristalise.kernel.common.ObjectNotFoundException;
 import org.cristalise.kernel.utils.Logger;
@@ -40,85 +43,81 @@ public class OutcomeValidator implements ErrorHandler {
 
     static SchemaValidator schemaValid = new SchemaValidator();
 
-    Schema schema;
+    Schema                      schema;
     javax.xml.validation.Schema xmlSchema;
-    protected StringBuffer errors = null;
+    protected StringBuffer      errors = null;
 
     public static OutcomeValidator getValidator(Outcome o) throws InvalidDataException, ObjectNotFoundException {
-    	Schema schema = o.getSchema(); 
-    	return getValidator(schema);
+        Schema schema = o.getSchema();
+        return getValidator(schema);
     }
-    
-    public static OutcomeValidator getValidator(Schema schema) throws InvalidDataException {
-    	
-        if (schema.getName().equals("Schema") &&
-        		schema.getVersion()==0)
-            return schemaValid;
 
-         return new OutcomeValidator(schema);
+    public static OutcomeValidator getValidator(Schema schema) throws InvalidDataException {
+        if (schema.getName().equals("Schema") && schema.getVersion() == 0) return schemaValid;
+
+        return new OutcomeValidator(schema);
     }
 
     protected OutcomeValidator() {
         errors = new StringBuffer();
     }
 
-	public OutcomeValidator(Schema schema) throws InvalidDataException {
-		this.schema = schema;
+    public OutcomeValidator(Schema schema) throws InvalidDataException {
+        this.schema = schema;
 
-        if (schema.getName().equals("Schema"))
-            throw new InvalidDataException("Use SchemaValidator to validate schema");
+        if (schema.getName().equals("Schema")) throw new InvalidDataException("Use SchemaValidator to validate schema");
 
         SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         schemaFactory.setErrorHandler(this);
+
         errors = new StringBuffer();
-        Logger.msg(5, "Parsing "+schema.getName()+" version "+schema.getVersion()+". "+schema.getSchemaData().length()+" chars");
+
+        Logger.msg(5, "OutcomeValidator() - Parsing "+schema.getName()+" version "+schema.getVersion()+". "+schema.getSchemaData().length()+" chars");
 
         try {
-        	xmlSchema = schemaFactory.newSchema(new StreamSource(new StringReader(schema.getSchemaData())));
-        } catch (SAXException ex) {
-            throw new InvalidDataException("Error parsing schema: "+ex.getMessage());
+            xmlSchema = schemaFactory.newSchema(new StreamSource(new StringReader(schema.getSchemaData())));
+        }
+        catch (SAXException ex) {
+            throw new InvalidDataException("Error parsing schema: " + ex.getMessage());
         }
 
-		if (errors.length() > 0) {
-            throw new InvalidDataException("Schema error: \n"+errors.toString());
-		}
+        if (errors.length() > 0) {
+            throw new InvalidDataException("Schema error: \n" + errors.toString());
+        }
 
-	}
+    }
 
-    public synchronized String validate(String outcome) {
-        if (outcome == null) return "Outcome String was null";
+    private synchronized String validate(Source outcome) {
         errors = new StringBuffer();
         try {
-        	Validator parser = xmlSchema.newValidator();
+            Validator parser = xmlSchema.newValidator();
             parser.setErrorHandler(this);
-
-            parser.validate(new StreamSource(new StringReader(outcome)));
-        } catch (Exception e) {
-            return e.getMessage();
+            parser.validate(outcome);
         }
+        catch (SAXException | IOException e) {
+            return "Couldn't create outcome validator:" + e.getMessage();
+        }
+
         return errors.toString();
     }
-    
-    public synchronized String validate(Document outcome) {
-        errors = new StringBuffer();
-        try {
-        	Validator parser = xmlSchema.newValidator();
-            parser.setErrorHandler(this);
 
-            parser.validate(new DOMSource(outcome));
-        } catch (Exception e) {
-            return "Couldn't create outcome validator:" +e.getMessage();
-        }
-        return errors.toString();
+    public String validate(String outcome) {
+        if (StringUtils.isBlank(outcome)) return "XML string was null or blank";
+
+        return validate(new StreamSource(new StringReader(outcome)));
+    }
+
+    public String validate(Document outcome) {
+        if (outcome == null) return "XML Document was null";
+
+        return validate(new DOMSource(outcome));
     }
 
     private void appendError(String level, Exception ex) {
-        errors.append(level);
-        String message = ex.getMessage();
-        
-        if (message == null || message.length() == 0) message = ex.getClass().getName();
+        errors.append("level:" + level);
 
-        errors.append(message);
+        if (StringUtils.isNotBlank(ex.getMessage())) errors.append(" msg:" + ex.getMessage());
+
         errors.append("\n");
     }
 
@@ -126,23 +125,23 @@ public class OutcomeValidator implements ErrorHandler {
      * ErrorHandler for instances
      */
     @Override
-	public void error(SAXParseException ex) throws SAXException {
-        appendError("ERROR: ", ex);
+    public void error(SAXParseException ex) throws SAXException {
+        appendError("ERROR", ex);
     }
 
     /**
      *
      */
     @Override
-	public void fatalError(SAXParseException ex) throws SAXException {
-        appendError("FATAL: ", ex);
+    public void fatalError(SAXParseException ex) throws SAXException {
+        appendError("FATAL", ex);
     }
 
     /**
      *
      */
     @Override
-	public void warning(SAXParseException ex) throws SAXException {
-        appendError("WARNING: ", ex);
+    public void warning(SAXParseException ex) throws SAXException {
+        appendError("WARNING", ex);
     }
 }
