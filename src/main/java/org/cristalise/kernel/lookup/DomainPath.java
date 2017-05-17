c2b1d05670ef62df72928dd57e275c6a3418c03f
Zsolt Kovacs <zs.myth@gmail.com>
2017-05-17 15:04:20 +0200

    removeing toString() as it is used in GUI a lot

--- src/main/java/org/cristalise/kernel/lookup/DomainPath.java
+++ src/main/java/org/cristalise/kernel/lookup/DomainPath.java
@@ -124,9 +124,4 @@ public class DomainPath extends Path {
 
     @Override
     public void setIOR(Object IOR) {}
-
-    @Override
-    public String toString() {
-        return super.toString() + " target:"+target;
-    }
 }
