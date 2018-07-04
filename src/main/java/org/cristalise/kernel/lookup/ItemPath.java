61d0642bdeaf4ed6afa63ff958d1327a4f2ee805
Zsolt Kovács <zs.myth@gmail.com>
2018-07-04 16:52:42 +0200

    #211 Fixed - change regex to validate UUIDs used by kernel resources

--- src/main/java/org/cristalise/kernel/lookup/ItemPath.java
+++ src/main/java/org/cristalise/kernel/lookup/ItemPath.java
@@ -182,6 +182,6 @@ public class ItemPath extends Path {
     public static boolean isUUID(String entityKey) {
         if (entityKey.startsWith("/entity/")) entityKey = entityKey.substring(8);
 
-        return entityKey.matches("^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$");
+        return entityKey.matches("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
     }
 }
