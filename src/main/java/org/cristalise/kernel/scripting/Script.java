fa0c737549e22fb21159f6736a51e55e0a7b7068
Rae Burawes <burawes.rae@gmail.com>
2018-07-05 12:03:56 +0200

    Issue #12: Map of ListOfValues - added comment to the line change

--- src/main/java/org/cristalise/kernel/scripting/Script.java
+++ src/main/java/org/cristalise/kernel/scripting/Script.java
@@ -676,7 +676,9 @@ public class Script implements DescriptionObject {
             if (outputValue != null && !(outputParam.getType().isInstance(outputValue)))  {
                 throw new ScriptingEngineException("Script "+getName()+" - output "+outputName+" was not null and it was not instance of " + outputParam.getType().getName() + ", it was a " + outputValue.getClass().getName());    
             }
-
+            
+            // if the outputValue is null and returnValue has value then use the return value instead
+            // e.g. returnValue is map of item name and UUID
             outputs.put(outputParam.getName(), outputValue != null ? outputValue : returnValue);
         }
         return outputs;
